c4d61d562204859f2213498464b9853c9364ee69
mariusz <budkagracza@gmail.com>
2018-06-11 01:08:44 +0200

    create views for tests uses

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.views import View
+from django.views.generic import FormView
+
+from app.forms import ImagerForm
+
+
+class UploadImageFormView(FormView):
+    form_class = ImagerForm
+
+
+class ImageView(View):
+    pass
