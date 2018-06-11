538e286be412044e28cc1ae69d009f37c8202800
mariusz <budkagracza@gmail.com>
2018-06-12 01:56:28 +0200

    add test for existing image

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,10 @@
 from io import BytesIO
 
 from PIL import Image
+from django.http import Http404
 from django.test import TestCase, RequestFactory
+
+from app.views import ImageView
 
 
 class ImagerTest(TestCase):
@@ -12,3 +15,17 @@ class ImagerTest(TestCase):
         self.existing_image = BytesIO(image.tostring())
         self.existing_image.name = 'green.png'
         self.existing_image.seek(0)
+
+    def not_existing_image(self):
+        request_not_existing_image = self.factory.get('/imager/image/red/')
+
+        with self.assertRaises(Http404):
+            response = ImageView.as_view(request_not_existing_image)
+
+    def existing_image(self):
+        request_existing_image = self.factory.get('/imager/image/green/')
+        response = ImageView.as_view(request_existing_image)
+        self.assertEqual(response.status_code, 200)
+
+
+# class ImagePathTest(TestCase):
