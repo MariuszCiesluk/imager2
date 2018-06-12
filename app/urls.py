1ee516d37b366fdb54fb74b02ddbf86062567ec3
mariusz <budkagracza@gmail.com>
2018-06-13 01:45:57 +0200

    modified urls and settings

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from app.views import UploadImageFormView, ImageView
+from app.views import UploadImageFormView, ImageView, MainPageView
 
 urlpatterns = [
+    path('', MainPageView.as_view(), name='main-page'),
     path('upload/', UploadImageFormView.as_view(), name='upload-image-form'),
     path('image/<str:name>/', ImageView.as_view(), name='image-detail'),
-    path('image/', ImageView.as_view(), name='image-detail'),
 
 ]
--- imager/urls.py
+++ imager/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import include
 from django.urls import path
 
 urlpatterns = [
-    path('imager/', include('imager.foo.urls')),
+    path('', include('app.urls')),
 
 ]
