a58dcf46d7cdc076bf6582e00b32b5bccb4d7408
mariusz <budkagracza@gmail.com>
2018-06-13 00:28:34 +0200

    tests for upload files

--- app/tests.py
+++ app/tests.py
@@ -1,10 +1,11 @@
+import os
 from io import BytesIO
 
 from PIL import Image
+from django.conf import settings
 from django.http import Http404
 from django.test import TestCase, RequestFactory
 from django.core.files.uploadedfile import InMemoryUploadedFile
-from StringIO import StringIO
 
 from app.constants import TEST_IMAGE
 from app.forms import ImagerForm
@@ -16,9 +17,13 @@ class ImagerTest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         image = Image.new('RGBA', size=(50, 50), color=(155, 0, 0))
-        self.existing_image = BytesIO(image.tostring())
-        self.existing_image.name = 'green.png'
-        self.existing_image.seek(0)
+        image.save(os.path.join(settings.MEDIA_ROOT, 'green.png'))
+
+    def tearDown(self):
+        try:
+            os.remove(os.path.join(settings.MEDIA_ROOT, 'green.png'))
+        except OSError:
+            pass
 
     def test_not_existing_image(self):
         request_not_existing_image = self.factory.get('/imager/image/red/')
@@ -35,15 +40,54 @@ class ImagerTest(TestCase):
 class ImageUploadTest(TestCase):
 
     def setUp(self):
-        self.image = InMemoryUploadedFile(
+        self.image_blue = InMemoryUploadedFile(
             BytesIO(TEST_IMAGE),
-            field_name='tempfile',
+            field_name='image',
             name='blue.png',
             content_type='image/png',
-            size=len(TEST_IMAGE),
+            size=(15, 15),
             charset='utf-8',
         )
+        self.image_green = InMemoryUploadedFile(
+            BytesIO(TEST_IMAGE),
+            field_name='image',
+            name='green.png',
+            content_type='image/png',
+            size=(5, 5),
+            charset='utf-8',
+        )
+        img = Image.new('RGB', (10, 10))
+        img.save(os.path.join(settings.MEDIA_ROOT, 'green.png'))
+
+    def tearDown(self):
+        try:
+            os.remove(os.path.join(settings.MEDIA_ROOT, 'green.png'))
+        except OSError:
+            pass
+
+        try:
+            os.remove(os.path.join(settings.MEDIA_ROOT, 'blue.png'))
+        except OSError:
+            pass
 
     def test_valid(self):
-        form = ImagerForm(files={'image': self.image})
+        form = ImagerForm(files={'image': self.image_green})
         self.assertEqual(form.is_valid(), True)
+
+    def test_new_file(self):
+        form = ImagerForm(files={'image': self.image_blue})
+        if form.is_valid():
+            form.save()
+            uploaded_image = Image.open(os.path.join(settings.MEDIA_ROOT, 'blue.png'))
+            self.assertEqual(uploaded_image.size, (15, 15))
+        else:
+            self.assertEqual(form.is_valid(), True)
+
+    def test_existing_file(self):
+        form = ImagerForm(files={'image': self.image_green})
+        if form.is_valid():
+            form.save()
+            uploaded_image = Image.open(os.path.join(settings.MEDIA_ROOT, 'green.png'))
+            self.assertEqual(uploaded_image.size, (5, 5))
+        else:
+            self.assertEqual(form.is_valid(), True)
